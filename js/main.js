ccd07584874425643b10e0a1906ce462699adede
Gabriel Mendes Rangel <gabrielmendesrangel2@gmail.com>
2023-04-14 21:21:41 -0300

    Projeto Finalizado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,38 @@
 const controle = document.querySelectorAll('[data-controle]');
+const estatistica = document.querySelectorAll('[data-estatistica]');
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+};
 
 controle.forEach( (elemento) => {
     elemento.addEventListener('click', (evento) => {
@@ -6,14 +40,28 @@ controle.forEach( (elemento) => {
     });
 });
 
-function manipulaDados(operacao, controle){
-    const peca = controle.querySelector('[data-contador]');
+function manipulaDados(tipoDeOperacao, paiDoAlvo){
+    const contador = paiDoAlvo.querySelector('[data-contador]');
+    const peca = paiDoAlvo.querySelector('[data-peca]');
     
-    if(operacao === "-"){
-        if(parseInt(peca.value) > 0){
-            peca.value = parseInt(peca.value) - 1;
+    if(tipoDeOperacao === "-"){
+        if(parseInt(contador.value) > 0){
+            contador.value = parseInt(contador.value) - 1;
         }
     } else {
-        peca.value = parseInt(peca.value) + 1;
+        contador.value = parseInt(contador.value) + 1;
     }
+    
+    atualizaEstatisticas(peca.dataset.peca, tipoDeOperacao);
 }
+
+
+function atualizaEstatisticas(peca, tipoDeOperacao){
+    estatistica.forEach( (elemento) => {
+        if(tipoDeOperacao === "-"){
+            elemento.textContent = parseInt(elemento.textContent) - pecas[peca][elemento.dataset.estatistica];
+        } else {
+            elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica];
+        }
+    });
+};
